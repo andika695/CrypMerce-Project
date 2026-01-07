ac5da24716f539f8a65c17b269203f6008f03076
andika695 <56471292+andika695@users.noreply.github.com>
2026-01-07 19:06:46 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,6 +1,10 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// Mengarahkan (Redirect) user langsung ke halaman login
+header("Location: user/dashboard.html");
+exit;
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
-echo "DEBUG OK";
+// echo "DEBUG OK";
+?>
